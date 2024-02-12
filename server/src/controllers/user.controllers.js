8685451c18630210e0829832ffceaccf1848e218
Akhil-Sharma-26 <2004akhil26sharma@gmail.com>
2024-02-12 17:15:07 +0530

    user controllers testing DONE

--- server/src/controllers/user.controllers.js
+++ server/src/controllers/user.controllers.js
@@ -11,69 +11,97 @@ const generateAccessToken = async (user) => {
         await user.save({ validateBeforeSave: false }); 
         return accessToken;
     } catch (error) {
-        throw new ApiError(500, "Token creation failed");
+        throw new Error(500, "Token creation failed");
     }
 }
 async function registerUser(req,res){
+    // console.log(req.body)
     const {username,password,email} = req.body
-    if(username && password && email){
-        throw new error('Some fields are not present');
+    if(!username && !password && !email){
+        throw new Error('Some fields are not present');
     }
-    try {
+    // console.log(username,password,email);
+    // try {
         const existedUser = await User.findOne({
             $or: [
                 { username },
                 { email }
             ]
         })
-    
+        // console.log(existedUser); // null if not exist
+        if(existedUser){
+            throw new Error('user already exists!!')
+        }
         const user = await User.create({
             username: username,
             email: email,
             password: password
         })
-        const CreatedUser = await User.findById(user._id).select("-password -accessToken");
+        // console.log(user);
+        const CreatedUser = await User.findById(user._id).select("-password -accessToken"); //
+        // This above method can be removed as it may be a burden db call
         if (!CreatedUser) { 
-            throw new ApiError(500, "User creation failed");
+            throw new Error(500, "User creation failed");
         }
+        // console.log(CreatedUser);
         return res.status(200).json({
             data: 'User created Successfully',
         })
-    } catch (error) {
-        console.log('user registration failed miserably');   
-    }
+    // } catch (error) {
+    //     console.log('user registration failed miserably');   
+    // }
 }
 
 async function LoginUser(req,res){
     try {
         const {email,password} = req.body;
-        if(email && password){
+        if(!email && !password){
             console.log('Something is missing');
         }
-        const user = User.findOne({
+        const user = await User.findOne({
             $or : [
                 {email},
                 {password}
             ]
         })
         if(!user){
-            throw new error(404,'User doenst exist in DB')
+            throw new Error(404,'User doenst exist in DB')
         }
-        const passValidorNot = User.passValidorNot(password)
-        if (!isPassValid) {
-            throw new error(401, "Password is incorrect");
+        const passValidorNot = await user.isPasswordCorrect(password)
+        if (!passValidorNot) {
+            throw new Error(401, "Password is incorrect");
         }
         const accessToken= await generateAccessToken(user); // I am tweeking the argument here, change it to prev state if some unexpected error appears 
-    
+        console.log(accessToken);
         return res.status(200).cookie("accessToken",accessToken,options).json({
-            data: 'Yoooo!!! User Registered'
+            data: 'Yoooo!!! User loggedIn successfull'
         })
     } catch (error) {
         console.log('A very serious error has poped out!!',error);
     }
 }
+async function logout(req,res){
+    await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $unset: {
+                refreshToken: 1 // this removes the field from document
+            }
+        },
+        {
+            new: true
+        }
+    )
 
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .json({
+        data: "Logout Successfull!!"
+    })
+}
 export {
     LoginUser,
-    registerUser
+    registerUser,
+    logout
 }
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from 'express'
 import {
     LoginUser,
+    logout,
     registerUser
 } from '../controllers/user.controllers.js'
 
@@ -8,6 +9,6 @@ const router = Router()
 
 router.route('/login').post(LoginUser)
 router.route('/signup').post(registerUser)
-
+router.route('/logout').post(logout)
 
 export default router
